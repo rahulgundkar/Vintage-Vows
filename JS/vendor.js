e912765d6a9be6d3ad46ef09a4f17af9d0dc5a42
rahulgundkar <rahulgundkar2137@gmail.com>
2024-04-15 15:38:26 +0530

    changes by rahul

---
+++ JS/vendor.js
@@ -0,0 +1,21 @@
+
+
+
+function setNewImage(){
+    document.getElementById("img").src ="../Images/vendor/js1.webp";
+  
+}
+function setOldImage(){
+    document.getElementById("img").src ="../Images/vendor/js2.webp";
+
+}
+function Light() {
+    const Element = document.getElementById("DarkMode");
+    Element.className = "body";
+}
+
+function Dark() {
+    const Element = document.getElementById("DarkMode");
+    Element.className = "Dark";
+}
+
