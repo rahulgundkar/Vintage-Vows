9e909a94ef53d8a6066dfeb11ae4092c3dc321a7
taihan khane <taihankhan86@gmail.com>
2024-05-10 20:41:37 +0530

    old file Added

---
+++ JS/Destination.js
@@ -0,0 +1,64 @@
+function setNewImage0(){
+    document.getElementById("img0").src ="../Images/Destination-Wedding-img/Goa-client/img-3-3.jpeg";
+
+}
+function setOldImage0(){
+    document.getElementById("img0").src ="../Images/Destination-Wedding-img/Goa-client/img-3.jpg";
+
+}
+
+function setNewImage(){
+    document.getElementById("img").src ="../Images/Destination-Wedding-img/Goa-client/img-1-1.jpg";
+
+}
+function setOldImage(){
+    document.getElementById("img").src ="../Images/Destination-Wedding-img/Goa-client/img-1.jpg";
+
+}
+
+function setNewImage1(){
+    document.getElementById("img1").src ="../Images/Destination-Wedding-img/Goa-client/img-2-2.jpg";
+
+}
+function setOldImage1(){
+    document.getElementById("img1").src ="../Images/Destination-Wedding-img/Goa-client/img-2.jpg";
+
+}
+function setNewImage2(){
+    document.getElementById("img2").src ="../Images/Destination-Wedding-img/Goa-client/img-4-4.jpg";
+
+}
+function setOldImage2(){
+    document.getElementById("img2").src ="../Images/Destination-Wedding-img/Goa-client/img-4.jpg";
+
+}
+function setNewImageJaipur(){
+    document.getElementById("Jaipur-img").src = "../Images/Destination-Wedding-img/jaipur-client/J-1.webp"
+}
+function setOldImageJaipur(){
+    document.getElementById("Jaipur-img").src = "../Images/Destination-Wedding-img/jaipur-client/J-1-1.webp"
+}
+function setNewImageJaiselmer(){
+    document.getElementById("jaisalmer-img").src = "../Images/Destination-Wedding-img/jaisalmer-client/Ja-1.webp"
+}
+function setOldImageJaiselmer(){
+    document.getElementById("jaisalmer-img").src = "../Images/Destination-Wedding-img/jaisalmer-client/Ja-1-1.jpg"
+}
+function setNewImageJodhpur(){
+    document.getElementById("jodhpur-img").src = "../Images/Destination-Wedding-img/jodhpur-clint/jodhpur-clint.webp"
+}
+function setOldImageJodhpur(){
+    document.getElementById("jodhpur-img").src = "../Images/Destination-Wedding-img/jodhpur-clint/jodhpur-clint-1-1.webp"
+}
+function setNewImagepushkar(){
+    document.getElementById("pushkar-img").src = "../Images/Destination-Wedding-img/pushkar-client/pushkar-1-1.webp"
+}
+function setOldImagepushkar(){
+    document.getElementById("pushkar-img").src = "../Images/Destination-Wedding-img/pushkar-client/pushkar-1.webp"
+}
+function setNewImageudaipur(){
+    document.getElementById("udaipur-img").src = "../Images/Destination-Wedding-img/udaipur-client/udaipur-clint-1-1.jpg"
+}
+function setOldImageudaipur(){
+    document.getElementById("udaipur-img").src = "../Images/Destination-Wedding-img/udaipur-client/udaipur-client-1.jpg"
+}
